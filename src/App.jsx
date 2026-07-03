069e6dcf50cf6c5ef8133b21e131ea20b90af070
Aman Singh <ta5638576@gmail.com>
2026-07-03 17:04:22 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import Home from './components/Home.jsx'
-import AboutUs from './components/AboutUs.jsx';
-import VisionMission from './components/VisionMission.jsx';
-import Amenities from './components/Amenities.jsx';
-import Testimonials from './components/Testimonials.jsx';
-import ContactUs from './components/ContactUs.jsx';
-import Footer from './components/Footer.jsx';
+import Home from './components/home.jsx'
+import AboutUs from './components/aboutUs.jsx';
+import VisionMission from './components/visionmission.jsx';
+import Amenities from './components/amenities.jsx';
+import Testimonials from './components/testimonials.jsx';
+import ContactUs from './components/contactUs.jsx';
+import Footer from './components/footer.jsx';
 
 export default function App() {
   return (
     <div className="bg-light min-vh-100 overflow-x-hidden">
-      <Home />
+      <home />
 
 
-
-      <AboutUs />
-      <VisionMission />
-      <Amenities />
-      <Testimonials />
-      <ContactUs />
-      <Footer />
+      <aboutUs />
+      <visionmission />
+     <amenities />
+      <testimonials />
+      <contactUs />
+      <footer />
     </div>
   );
 }
